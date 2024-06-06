e54fc46efecdc32d73bae85cfa628839d9a312a7
kaushalrajbacancy <kaushalrajsinh.hada@bacancy.com>
2024-06-06 17:53:38 +0530

    Fix default contract addresses and bundlerApiKey

--- examples/01-get-address.ts
+++ examples/01-get-address.ts
@@ -5,7 +5,7 @@ dotenv.config();
 
 
 async function main() {
-  const bundlerApiKey = 'eyJvcmciOiI2NTIzZjY5MzUwOTBmNzAwMDFiYjJkZWIiLCJpZCI6ImY4NTVhZGIxODliNDRlODJhMzhkZWZmODY0ZmEzZGMzIiwiaCI6Im11cm11cjEyOCJ9';
+  const bundlerApiKey = 'eyJvcmciOiI2NTIzZjY5MzUwOTBmNzAwMDFiYjJkZWIiLCJpZCI6IjMxMDZiOGY2NTRhZTRhZTM4MGVjYjJiN2Q2NDMzMjM4IiwiaCI6Im11cm11cjEyOCJ9';
   const customBundlerUrl = '';
   // initializating sdk...
   const modularSdk = new ModularSdk({ privateKey: process.env.WALLET_PRIVATE_KEY }, { chainId: Number(process.env.CHAIN_ID), bundlerProvider: new EtherspotBundler(Number(process.env.CHAIN_ID), bundlerApiKey, customBundlerUrl) }) // Testnets dont need apiKey on bundlerProvider
