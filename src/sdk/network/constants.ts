675e5daf059174c7a271bb976bd186745df4a84b
kaushalrajbacancy <kaushalrajsinh.hada@bacancy.com>
2024-06-14 12:21:55 +0530

    Remove XDC testnet from the Networks

--- src/sdk/network/constants.ts
+++ src/sdk/network/constants.ts
@@ -33,11 +33,10 @@ export enum NetworkNames {
   Ancient8Testnet = 'ancient8Testnet',
   Ancient8 = 'ancient8',
   Amoy = 'amoy',
-  XDCTestnet = 'xdcTestnet',
 }
 
 export const SupportedNetworks =
-  [1, 10, 14, 30, 31, 51, 56, 97, 100, 114, 122, 123, 137, 2357, 5000, 5003, 8453, 10200, 20197, 42161, 43113, 43114, 59140, 59144, 80002, 84532, 421614, 534351, 534352, 11155111, 11155420, 28122024, 888888888]
+  [1, 10, 14, 30, 31, 56, 97, 100, 114, 122, 123, 137, 2357, 5000, 5003, 8453, 10200, 20197, 42161, 43113, 43114, 59140, 59144, 80002, 84532, 421614, 534351, 534352, 11155111, 11155420, 28122024, 888888888]
 
 export const NETWORK_NAME_TO_CHAIN_ID: {
   [key: string]: number;
@@ -74,7 +73,6 @@ export const NETWORK_NAME_TO_CHAIN_ID: {
   [NetworkNames.Ancient8Testnet]: 28122024,
   [NetworkNames.Ancient8]: 888888888,
   [NetworkNames.Amoy]: 80002,
-  [NetworkNames.XDCTestnet]: 51,
 };
 
 export const onRamperAllNetworks = ['OPTIMISM', 'POLYGON', 'ARBITRUM', 'FUSE', 'GNOSIS', 'ETHEREUM']
@@ -390,16 +388,6 @@ export const Networks: {
       walletFactory: '0xf80D543Ca10B48AF07c65Ff508605c1737EFAF3F',
       bootstrap: '0x1baCB2F1ef4fD02f02e32cCF70888D9Caeb5f066',
       multipleOwnerECDSAValidator: '0x8c4496Ba340aFe5ac4148cfEA9ccbBCD54093143',
-    },
-  },
-  [51]: {
-    chainId: 51,
-    bundler: 'https://testnet-rpc.etherspot.io/v2/51',
-    contracts: {
-      entryPoint: '0x0000000071727De22E5E9d8BAf0edAc6f37da032',
-      walletFactory: '0x3aa509fd07B09d5a8b944c96ae6eE767fe75C465',
-      bootstrap: '0xCF9bf036D63641003566FD8B10817Dd7168ac5Cb',
-      multipleOwnerECDSAValidator: '0x61a5eb96F597c72cd24a32D4e241fCA5b22f3dDb',
     },
   }
 };
