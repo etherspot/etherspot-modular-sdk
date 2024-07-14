7c7cb392e4a5cef3d238fd15ee2a37aa2c25ae33
kanth <kanth.explorer@gmail.com>
2024-07-14 11:48:36 +0530

    feat: PRO-2373 hexlify customised function added

--- src/sdk/common/ERC4337Utils.ts
+++ src/sdk/common/ERC4337Utils.ts
@@ -1,6 +1,6 @@
 import { BytesLike, defaultAbiCoder, hexlify } from 'ethers/lib/utils';
 import { UserOperationStruct } from '../contracts/account-abstraction/contracts/core/BaseAccount';
-import { BigNumber, BigNumberish, ethers } from 'ethers';
+import { BigNumber, BigNumberish } from 'ethers';
 import { Buffer } from 'buffer';
 import { concat, encodeAbiParameters, Hex, keccak256, pad, parseAbiParameters, toHex } from 'viem';
 
@@ -196,64 +196,6 @@ export function rethrowError(e: any): any {
   }
   throw e;
 }
-
-
-// export function hexlify(value: BytesLike | Hexable | number | bigint, options?: DataOptions): string {
-//   if (!options) { options = { }; }
-
-//   if (typeof(value) === "number") {
-//       logger.checkSafeUint53(value, "invalid hexlify value");
-
-//       let hex = "";
-//       while (value) {
-//           hex = HexCharacters[value & 0xf] + hex;
-//           value = Math.floor(value / 16);
-//       }
-
-//       if (hex.length) {
-//           if (hex.length % 2) { hex = "0" + hex; }
-//           return "0x" + hex;
-//       }
-
-//       return "0x00";
-//   }
-
-//   if (typeof(value) === "bigint") {
-//       value = value.toString(16);
-//       if (value.length % 2) { return ("0x0" + value); }
-//       return "0x" + value;
-//   }
-
-//   if (options.allowMissingPrefix && typeof(value) === "string" && value.substring(0, 2) !== "0x") {
-//        value = "0x" + value;
-//   }
-
-//   if (isHexable(value)) { return value.toHexString(); }
-
-//   if (isHexString(value)) {
-//       if ((<string>value).length % 2) {
-//           if (options.hexPad === "left") {
-//               value = "0x0" + (<string>value).substring(2);
-//           } else if (options.hexPad === "right") {
-//               value += "0";
-//           } else {
-//               logger.throwArgumentError("hex data is odd-length", "value", value);
-//           }
-//       }
-//       return (<string>value).toLowerCase();
-//   }
-
-//   if (isBytes(value)) {
-//       let result = "0x";
-//       for (let i = 0; i < value.length; i++) {
-//            let v = value[i];
-//            result += HexCharacters[(v & 0xf0) >> 4] + HexCharacters[v & 0x0f];
-//       }
-//       return result;
-//   }
-
-//   throw new Error(`invalid hexlify value: ${value}`);
-// }
 
 /**
  * hexlify all members of object, recursively
