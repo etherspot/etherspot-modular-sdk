2dad32343447ddb8db13a888a7d9f7e05cfa7935
kanth <kanth.explorer@gmail.com>
2024-07-30 12:48:55 +0530

    fix: PRO-2373 build as cjs and esm and tsconfig updates

--- src/sdk/network/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './interfaces';
-export * from './network.service';
-export * from './utils';
-export { NetworkNames } from './constants';
--- src/sdk/network/utils/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './prepare-network-name';
-export * from './network-name-to-chain-id';
