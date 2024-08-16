f60ce8ca55b5c73efa9b42e6339a498251668c84
kanth <kanth.explorer@gmail.com>
2024-08-16 12:57:12 +0530

    fix: update repo package name and missing dependencies in main index

--- src/sdk/index.ts
+++ src/sdk/index.ts
@@ -5,6 +5,7 @@ export * from './dto';
 export * from './interfaces';
 export * from './network';
 export * from './bundler';
+export * from './common';
 
 export { ModularSdk, SessionKeyValidator };
 export default ModularSdk;
