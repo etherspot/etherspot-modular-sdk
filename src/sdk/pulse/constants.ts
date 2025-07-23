a70c9b665254191388afbbea424291515e42ec0e
cryptonoyaiba <71776468+cryptonoyaiba@users.noreply.github.com>
2025-07-23 11:57:49 +0100

    Update constants.ts
    - Fix for incorrect HookType assignment

--- src/sdk/pulse/constants.ts
+++ src/sdk/pulse/constants.ts
@@ -1,7 +1,7 @@
 export enum HookType {
-  GLOBAL = '0x01',
-  DELEGATECALL = '0x02',
-  VALUE = '0x03',
-  SIG = '0x04',
-  TARGET_SIG = '0x05',
+  GLOBAL = '0x00',
+  DELEGATECALL = '0x01',
+  VALUE = '0x02',
+  SIG = '0x03',
+  TARGET_SIG = '0x04',
 }
